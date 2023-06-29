0930576db0d25e6b818dfb62328dc110551128f6
ramalloc <roshangupta1887@gmail.com>
2023-06-29 14:55:07 +0530

    Updated App.jsx by adding Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import './App.css';
 import HomePage from './Pages/HomePage';
-
+import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import Cart from "./Pages/Cart";
+import {
+BrowserRouter as Router,
+Switch,
+Route,
+Redirect
+} from "react-router-dom";
 function App() {
+  const user = true;
   return (
-    <>
-    <HomePage />
-    </>
+    <Router>
+      <Switch>
+
+    <Route path = "/" exact>
+      <HomePage />
+    </Route>
+    <Route path = "/products/:id">
+      <ProductList />
+    </Route>
+    <Route path = "/cart">
+      <Cart />
+    </Route>
+    <Route path = "/login">
+      {user ? <Redirect to = "/" /> : <Login />}
+    </Route>
+    <Route path = "/register">
+      {user ? <Redirect to = "/" /> : <Register/>}
+    </Route>
+
+
+      </Switch>
+    </Router>
   );
 }
 
